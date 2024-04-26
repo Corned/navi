2d583bb0bb5dd98df6934cfde948eebbc937a96d
Oliver K <Corned@users.noreply.github.com>
2024-04-26 09:04:40 +0300

    added .env file for firebase config

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -21,6 +27,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getFirestore(app)
+
+console.log(process.env);
 
 /* const getLinks = async () => {
   const linkCol = collection(db, "private-collection")
